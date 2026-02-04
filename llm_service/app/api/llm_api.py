040f22dfe5b5bb79ff4f2bceb96a1d7eab782150
Andrew Beaton <andrewbeaton123@gmail.com>
2026-02-04 22:40:14 +0000

    Pre api branch commit

--- llm_service/app/api/llm_api.py
+++ llm_service/app/api/llm_api.py
@@ -1,8 +1,22 @@
 
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(
+    prefix = "/api/v1",
+    tags=["llm"]
+)
 
 @router.get("/ping")
 def ping():
     return {"message" : "pong"}
+
+
+@router.get("/health")
+def health():
+    return {"message" : "ok"}
+
+
+@router.get("/generate")
+def generate():
+    
+    return {"text": "Test Response !"}
--- llm_service/app/main.py
+++ llm_service/app/main.py
@@ -1,11 +1,8 @@
 
 from fastapi import FastAPI
 from llm_service.app.api.llm_api import router as llm_router
+
+
+
 app = FastAPI()
-
-@app.get("/health")
-def health(): 
-    return {"status":"ok"}
-
-
 app.include_router(llm_router)
