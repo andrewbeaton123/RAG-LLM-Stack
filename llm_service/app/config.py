3937fc9abf4fb352efadf701e360c643ed4f4bd8
Andrew Beaton <andrewbeaton123@gmail.com>
2026-04-26 20:02:19 +0100

    Basic singleton config class added and coded. Added loguru to this as well. The default config is still to be written.

---
+++ llm_service/app/config.py
@@ -0,0 +1,40 @@
+import yaml
+from typing import Optional, Any
+from pathlib import Path
+from loguru import logger
+
+class Config():
+
+    instance = None
+
+    def __new__(cls):
+        if cls.instance is None:
+            cls.instance =  super().__new__(cls)
+        return cls.instance
+
+    def _load_config(self,
+                    path :Optional[str]= None):
+        
+        if path is None: 
+            path = Path(__file__).resolve().parent/"defualt_config.yml"
+
+
+        try :
+            with open(path) as f : 
+                self._dict = yaml.safe_load(f) or {}
+        
+        except FileNotFoundError:
+            logger.error(f"Config file not found at {path}")
+            self._dict = {}
+        
+        except yaml.YAMLError as e :
+            logger.error(f" Error parsing the yaml file {e}")
+            self._dict = {}
+
+        
+    def get(self, key : str , default: Any = None) -> Any: 
+        return self._dict.get(key, default)
+    
+    @property
+    def all (self):
+        return self._dict
